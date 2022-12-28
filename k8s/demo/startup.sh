66d07ffc77c2f735b1cac49537f9177beed90496
Minh Luu <minhltuit@gmail.com>
2022-12-28 18:27:10 +0700

    update flutter startup

--- k8s/demo/startup.sh
+++ k8s/demo/startup.sh
@@ -2,6 +2,8 @@
 
 tmux new -d -s webSession
 
+tmux pipe-pane -o -t webSession 'cat >> /tmp/tmuxpipe'
+
 tmux send-keys -t webSession.0 "(cd /app && /usr/local/flutter/bin/flutter build web &&  /usr/local/flutter/bin/flutter run --web-port 5000 -d web-server --web-hostname 0.0.0.0)" ENTER
 
 tmux new -d -s webReloader
